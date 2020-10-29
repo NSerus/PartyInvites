afe67f762ad09416e2ccab62014ffc06e938aff2
NSerus <nunoworkingserus@gmail.com>
2020-10-29 11:27:06 +0000

    Updated Validation

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -9,12 +9,14 @@ namespace PartyInvites.Models
     public class GuestResponse
     {
         [Required(ErrorMessage = "Please enter your name")]
+        [StringLength(80, MinimumLength = 2, ErrorMessage = "The Name should have at leat twa chars and a maximum of 80")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Please enter your phone")]
         public string Phone { get; set; }
 
         [Required(ErrorMessage = "Please enter a valid email")]
+        [EmailAddress(ErrorMessage = "Please enter better")]
         public string Email { get; set; }
 
         public bool? WillAttend { get; set; }
